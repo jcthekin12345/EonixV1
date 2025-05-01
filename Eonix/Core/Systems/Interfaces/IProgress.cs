6819a4e72bab30787330caeead9b4de323bbc523
Jctheking <93554396+jcthekin12345@users.noreply.github.com>
2025-05-01 04:38:51 +0200

    feat: add Name property and DisplayInfo method to IProgress interface

--- Eonix/Core/Systems/Interfaces/IProgress.cs
+++ Eonix/Core/Systems/Interfaces/IProgress.cs
@@ -5,5 +5,11 @@ namespace Eonix.Core.Systems.Interfaces;
 public interface IProgress<T>
 {
     T Value { get; set; }
+    string? Name { get; set; }
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"{Name}: {Value}");
+    }
     
 }
