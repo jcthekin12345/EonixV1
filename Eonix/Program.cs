1d56fa0fc8758389a7788c9d4fc22486414a9e14
Jctheking <93554396+jcthekin12345@users.noreply.github.com>
2025-05-02 04:14:52 +0200

    feat: integrate ProducerUtils to modify producer names dynamically`
    Signed-off-by: Jctheking <93554396+jcthekin12345@users.noreply.github.com>

--- Eonix/Program.cs
+++ Eonix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Eonix.Core.Systems.Producers;
+using Eonix.Core.Utils;
 
 namespace Eonix;
 
@@ -10,9 +11,10 @@ class Program
         upgradeShop[0] = new Producer("Friend", "A good buddy to help ya along the way", 0.5, 0.2);
         upgradeShop[1] = new Producer("Worker", "A good office worker", 100, 0.3);
         upgradeShop[2] = new Producer("Manager", "Will take charge of your company", 150, 0.4);
-
+        var num = 0;
         foreach (var upgrades in upgradeShop)
         {
+            ProducerUtils.EditName(upgrades, $"Batman {num++ + 1}");
             ShowLn($"\nPrices: {upgrades.GetUpgradeCost():C}");
             ShowLn(upgrades);
         }
