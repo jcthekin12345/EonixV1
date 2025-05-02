de842797096f30609eda11159ed52ce169604871
Jctheking <93554396+jcthekin12345@users.noreply.github.com>
2025-05-02 04:01:16 +0200

    feat: implement producer management with sorting and enumeration
    Signed-off-by: Jctheking <93554396+jcthekin12345@users.noreply.github.com>

--- Eonix/Managers/ProducerManager.cs
+++ Eonix/Managers/ProducerManager.cs
@@ -1,5 +1,23 @@
+using System.Collections;
+using Eonix.Core.Systems.Producers;
+
 namespace Eonix.Managers;
 
-public class ProducerManager
+public class ProducerManager : IEnumerable<Producer>
 {
+    readonly List<Producer> producers = new();
+
+    public IEnumerator<Producer> GetEnumerator() => producers.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void AddProducer(Producer producer)
+    {
+        producers.Add(producer);
+    }
+
+    public void SortProducersByPrice()
+    {
+        producers.Sort(); // uses the IComparable implementation in Producer
+    }
 }
