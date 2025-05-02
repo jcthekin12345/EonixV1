e94575a40b44592adafbea486f6ff8324d6930c8
Jctheking <93554396+jcthekin12345@users.noreply.github.com>
2025-05-02 02:39:59 +0200

    fix: standardize variable naming convention to camelCase for upgradeShop

--- Eonix/Program.cs
+++ Eonix/Program.cs
@@ -7,12 +7,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Producer[] UpgradeShop = new Producer[3];
-        UpgradeShop[0] = new ("Friend", "A good buddy to help ya along the way",10.5, 0.1);
-        UpgradeShop[1] = new("Worker", "A good office worker", 100, 0.5);
-        UpgradeShop[2] = new("Manager", "Will take charge of your company", 150, 0.9);
+        Producer[] upgradeShop = new Producer[3];
+        upgradeShop[0] = new ("Friend", "A good buddy to help ya along the way",10.5, 0.1);
+        upgradeShop[1] = new("Worker", "A good office worker", 100, 0.5);
+        upgradeShop[2] = new("Manager", "Will take charge of your company", 150, 0.9);
 
-        foreach (var upgrades in UpgradeShop)
+        foreach (var upgrades in upgradeShop)
         {
             ShowLn(upgrades);
         }
